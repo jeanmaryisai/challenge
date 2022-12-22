8aee9a0ee21b8afe19551519f7982ddf9c06eab9
jeanmaryisai <jeanmaryisai@gmail.com>
2022-12-22 18:06:32 -0500

    deployemenr to railway test 1.3

--- challenge/settings.py
+++ challenge/settings.py
@@ -160,7 +160,7 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 #     MEDIA_URL = 'images/'
 
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

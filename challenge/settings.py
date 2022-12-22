ac22bc64aea92e29435961e57d225c30887e3c8e
jeanmaryisai <jeanmaryisai@gmail.com>
2022-12-22 16:20:19 -0500

    deployemenr to railway test 1.1

--- challenge/settings.py
+++ challenge/settings.py
@@ -154,6 +154,8 @@ MEDIA_ROOT= os.path.join(BASE_DIR,'static/images')
 if DEBUG:
     MEDIA_URL = 'images/'
 
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
